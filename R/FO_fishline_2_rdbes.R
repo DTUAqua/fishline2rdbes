0eaf0e65b96e11730fbd856b9ab61fde1dcbdb51
Kirsten Birch Håkansson <kibi@dtu.dk>
2021-09-30 23:55:39 +0200

    1st commit

---
+++ R/FO_fishline_2_rdbes.R
@@ -0,0 +1,255 @@
+
+#' FishLine 2 RDBES, Fishing trip (FT)
+#'
+#' @description Converts samples data from national database (fishLine) to RDBES.
+#' Data model v. 1.19
+#'
+#' @param path_to_data_model_baseTypes Where to find the baseTypes for the data model
+#' @param year years needed
+#' @param cruises Name of cruises in national database
+#' @param type only_mandatory | everything
+#'
+#' @author Kirsten Birch Håkansson, DTU Aqua
+#'
+#' @return
+#' @export
+#'
+#'
+#' @examples
+#'
+#'
+#'
+
+FO_fishline_2_rdbes <-
+  function(data_model_baseTypes_path = "Q:/mynd/RDB/create_RDBES_data/references",
+           years = 2016,
+           cruises = c("MON", "SEAS", "IN-HIRT"),
+           type = "everything"
+           )
+  {
+
+
+    # Input for testing ----
+
+    data_model_baseTypes_path <- "Q:/mynd/kibi/RDBES/create_RDBES_data/references"
+    years <- c(2018:2020)
+    cruises <- c("MON",  "IN-LYNG")
+    type <- "everything"
+
+    # Set-up ----
+
+    # agg_level <- "H"
+    library(sqldf)
+    library(dplyr)
+    library(stringr)
+    library(haven)
+
+    data_model <- readRDS(paste0(data_model_baseTypes_path, "/BaseTypes.rds"))
+
+    fo_temp <- filter(data_model, substr(name, 1, 2) == "FO")
+    fo_temp_t <- c("FOrecordType", t(fo_temp$name)[1:nrow(fo_temp)])
+
+    # Get needed stuff ----
+
+    # Get data from FishLine
+    channel <- odbcConnect("FishLineDW")
+    samp <- sqlQuery(
+      channel,
+      paste(
+        "select * FROM dbo.Sample
+         WHERE (Sample.year between ", min(years), " and ", max(years) , ")
+                and Sample.cruise in ('", paste(cruises, collapse = "','"),
+        "')",
+        sep = ""
+      )
+    )
+    close(channel)
+
+    channel <- odbcConnect("FishLine")
+    area <- sqlQuery(
+      channel,
+      paste(
+        "SELECT DFUArea, areaICES FROM L_DFUArea",
+        sep = ""
+      )
+    )
+    close(channel)
+
+
+
+    # Add needed stuff ----
+
+    fo <- left_join(samp, area, by = c("dfuArea" = "DFUArea"))
+
+
+    # Recode for FO ----
+
+    fo <- fo
+
+    fo$FOid <- fo$sampleId
+    fo$FOrecordType <- "FO"
+
+    fo$FOsequenceNumber <- fo$station  #To be coded manual - depends on design
+    fo$FOstratification <- "N"  #To be coded manual - depends on design
+    fo$FOstratumName <- "U"     #To be coded manual - depends on design
+    fo$FOclustering <- "N"         #To be coded manual - depends on design
+    fo$FOclusterName <- "No"     #To be coded manual - depends on design
+
+    fo$FOsampler[fo$cruise %in% c("MON", "SEAS")] <- "Observer"
+    fo$FOsampler[substr(fo$cruise, 1, 3) %in% c("BLH", "BRS", "MAKK", "SIL", "SPE", "TBM")] <-
+      "Self-Sampling"
+
+    fo$FOaggregationLevel <- agg_level
+
+    fo$FOvalidity <- fo$gearQuality
+    fo$FOvalidity [is.na(fo$FOvalidity)] <- "I"
+
+    fo$FOcatchReg[fo$cruise %in% c("MON", "SEAS") & fo$catchRegistration == "ALL" & fo$speciesRegistration == "ALL"] <- "All"
+    fo$FOcatchReg[fo$cruise %in% c("MON", "SEAS") & fo$catchRegistration == "DIS" & fo$speciesRegistration == "ALL"] <- "Dis"
+    fo$FOcatchReg[fo$cruise %in% c("MON", "SEAS") & fo$catchRegistration == "LAN" & fo$speciesRegistration == "ALL"] <- "Lan"
+    fo$FOcatchReg[fo$cruise %in% c("MON", "SEAS") & fo$catchRegistration == "NON" | fo$speciesRegistration != "ALL"] <- "None"
+    fo$FOcatchReg[fo$cruise %in% c("MON", "SEAS") & is.na(fo$catchRegistration) | is.na(fo$speciesRegistration)] <- "None"
+    fo$FOcatchReg[substr(fo$cruise, 1, 3) %in% c("BLH", "BRS", "MAKK", "SIL", "SPE", "TBM")] <- "Lan"
+
+    test <-
+      summarise(
+        group_by(fo, cruise, catchRegistration, speciesRegistration, FOcatchReg),
+        no_hauls = length(unique(sampleId))
+      )
+
+    # Start agg levels
+
+    fo_t <- subset(fo, tripType == "HVN")
+    fo_h <- subset(fo, tripType == "SØS")
+
+    # Haul level
+    fo_t$FOaggregationLevel <- "H"
+
+    fo_h$FOstartDate <- as.character(as.Date(fo_h$dateGearStart))
+    fo_h$FOstartTime <-
+      as.character(strftime(fo_h$dateGearStart, format = "%H:%M"))
+    fo_h$FOendDate <- as.Date(fo_h$dateGearEnd)
+    fo_h$FOendTime <- as.character(strftime(fo_h$dateGearEnd, format = "%H:%M"))
+
+    fo_h$FOduration[fo_h$cruise %in% c("MON", "SEAS")] <-
+      as.character(round(as.numeric(fo_h$fishingtime), digits = 0))
+
+    fo_h$FOdurationSource[fo_h$cruise %in% c("MON", "SEAS")] <-
+      "Crew"
+    fo_h$FOdurationSource[substr(fo_h$cruise, 1, 3) %in% c("BLH", "BRS", "MAKK", "SIL", "SPE", "TBM")] <-
+      "Data"
+
+    fo_h$FOstartLat <- as.character(round(fo_h$latPosStartDec, digits = 5))
+    fo_h$FOstartLon <- as.character(round(fo_h$lonPosStartDec, digits = 5))
+    fo_h$FOstopLat <- as.character(round(fo_h$latPosEndDec, digits = 5))
+    fo_h$FOstopLon <- as.character(round(fo_h$lonPosEndDec, digits = 5))
+
+    fo_h$FOfishingDepth <- as.character(round(fo_h$depthAveGear, digits = 0))
+    fo_h$FOfishingDepth[is.na(fo_h$FOfishingDepth)] <- ""
+
+    # Trip level
+    fo_t$FOaggregationLevel <- "T"
+
+    fo_t$FOstartDate <- ""
+    fo_t$FOstartTime <- ""
+    fo_t$FOendDate <- as.Date(fo_t$dateGearEnd)
+    fo_t$FOendTime <- ""
+
+    fo_t$FOduration <- ""
+    fo_t$FOdurationSource <- "Data"
+
+    fo_t$FOstartLat <- ""
+    fo_t$FOstartLon <- ""
+    fo_t$FOstopLat <- ""
+    fo_t$FOstopLon <- ""
+
+    fo_t$FOfishingDepth <- ""
+
+    fo <- bind_rows(fo_h, fo_t)
+
+    # End agg levels
+
+    fo$FOhandlingTime <- ""
+
+    fo$FOexclusiveEconomicZoneIndicator <- ""
+
+    fo$FOarea <- fo$areaICES
+    fo$FOrectangle <- fo$statisticalRectangle
+    fo$FOrectangle[is.na(fo$FOrectangle)] <- ""
+    fo$FOgsaSubarea <- "NotApplicable"
+    fo$FOjurisdictionArea <- ""
+
+    fo$FOwaterDepth <- ""
+
+    fo$FOnationalFishingActivity <- ""
+    fo$FOmetier5 <- ""
+    fo$FOmetier6 <- "MIS_MIS_0_0_0"
+    fo$FOgear <- fo$gearType
+    fo$FOmeshSize <- fo$meshSize
+    fo$FOselectionDevice <- ""
+    fo$FOselectionDeviceMeshSize <- ""
+    fo$FOtargetSpecies <- ""
+
+    fo$FOincidentalByCatchMitigationDeviceFirst <- "NotRecorded"
+    fo$FOincidentalByCatchMitigationDeviceTargetFirst <- "NotApplicable"
+    fo$FOincidentalByCatchMitigationDeviceSecond <- "NotRecorded"
+    fo$FOincidentalByCatchMitigationDeviceTargetSecond <- "NotApplicable"
+
+    fo$FOgearDimensions <- ""
+
+    fo$FOobservationCode <- "NotRecorded"
+
+    fo$FOnumberTotal <- ""      #To be coded manual - depends on design
+    fo$FOnumberSampled <- ""    #To be coded manual - depends on design
+    fo$FOselectionProb <- ""    #To be coded manual - depends on design
+    fo$FOinclusionProb <- ""    #To be coded manual - depends on design
+    fo$FOselectionMethod <- "NotApplicable"  #To be coded manual - depends on design
+
+    fo$FOunitName <- paste(fo$cruise, fo$trip, fo$station, sep = "-")
+
+    fo$FOselectionMethodCluster <- ""  #To be coded manual - depends on design
+    fo$FOnumberTotalClusters <- ""     #To be coded manual - depends on design
+    fo$FOnumberSampledClusters <- ""   #To be coded manual - depends on design
+    fo$FOselectionProbCluster <- ""    #To be coded manual - depends on design
+    fo$FOinclusionProbCluster <- ""       #To be coded manual - depends on design
+
+    fo$FOsampled <- "Y"
+    fo$FOreasonNotSampled <- ""
+
+    fo$FOsampled[fo$gearQuality == "I"] <- "N"
+    fo$FOreasonNotSampled[fo$gearQuality == "I"] <- "Other"
+
+    fo$FOsampled[fo$catchRegistration == "NON"] <- "N"
+    fo$FOreasonNotSampled[fo$catchRegistration == "NON"] <- "Other"
+
+    fo$FOsampled[fo$speciesRegistration %in% c("NON", "PAR")] <- "N"
+    fo$FOreasonNotSampled[fo$speciesRegistration %in% c("NON", "PAR")] <- "Other"
+
+    test_2 <-
+      summarise(
+        group_by(fo, cruise, gearQuality, catchRegistration, speciesRegistration, FOsampled),
+        no_hauls = length(unique(sampleId))
+      )
+
+    # For now only including Y - N requires manual coding
+    # Should be 0, but that is not possible at the moment
+    # fo$FOsampled[ft$cruise %in% c("MON", "SEAS") & is.na(ft$numberOfHaulsOrSets)] <- 0
+
+
+    if (type == "only_mandatory") {
+      ft_temp_optional <-
+        filter(data_model, substr(name, 1, 2) == "FT" & min == 0)
+      ft_temp_optional_t <-
+        factor(t(ft_temp_optional$name)[1:nrow(ft_temp_optional)])
+
+      for (i in levels(ft_temp_optional_t)) {
+        eval(parse(text = paste0("ft$", i, " <- ''")))
+
+      }
+    }
+
+    FO <- select(fo, one_of(fo_temp_t), tripId, sampleId, FOid, year)
+
+    return(list(FO, fo_temp, fo_temp_t))
+
+  }
