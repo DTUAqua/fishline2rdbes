1d73d753d298a3247eeec37b64c9e93ad1c4a499
Kirsten Birch Håkansson, DTU Aqua <kih@aqua.dtu.dk>
2023-09-14 19:19:00 +0200

    Adding a dateset to gear_info function

---
+++ data-raw/create_data_for_testing_fun_get_gear_info.R
@@ -0,0 +1,33 @@
+
+
+# Script for creating data_for_testing_fun_get_gear_info
+library(usethis)
+library(dplyr)
+library(RODBC)
+
+channel <- RODBC::odbcConnect("FishLineDW")
+samp <- RODBC::sqlQuery(
+  channel,
+  paste(
+    "select Sample.*, Trip.* FROM Sample INNER JOIN
+                  Trip ON Sample.tripId = Trip.tripId
+         WHERE (Trip.year = 2022)",
+    sep = ""
+  )
+)
+close(channel)
+
+samp <- subset(samp, !(substr(cruise, 1, 2) %in% c("BV", "To", "IB", "IE", "HE", "ME", "BI", "KA", "Tu")))
+
+unique(samp$cruise)
+
+# Mask cruise, trip, station
+
+samp$cruise <- "CRUISE"
+samp$trip <- "10"
+samp$station <- "1"
+
+data_for_testing_fun_get_gear_info <- select(samp, sampleId, year, cruise, trip, station, dfuArea, gearType, targetSpecies1, meshSize, selectionDevice,
+                 numberTrawls, numNets, heightNets, lengthNets)
+
+usethis::use_data(data_for_testing_fun_get_gear_info, overwrite = T)
