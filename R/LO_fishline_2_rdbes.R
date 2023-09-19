662fd56f262cb6530884d8b13ec7e935ddbb06fc
Kirsten Birch Håkansson, DTU Aqua <kih@aqua.dtu.dk>
2023-09-19 08:25:59 +0200

    Updated to new data model. This needs to be split into two funs

--- R/LO_fishline_2_rdbes.R
+++ R/LO_fishline_2_rdbes.R
@@ -20,6 +20,41 @@
 #'
 #'
 
+
+getDataModel <- function(tableName) {
+  ## datamodel
+  library(openxlsx)
+
+  nam <- data.frame(name = c("Design", "Location", "Temporal Event",
+                             "Species Selection",  "Landing event", "Sample",
+                             "Frequency Measure", "Biological Variable"),
+                    code = c("DE", "LO", "TE", "SS", "LE", "SA", "FM", "BV"))
+
+  if (! tableName %in% nam$name){
+    print("Table Name Not Found In CS Design Model")
+  } else{
+
+    j <- match(tableName, nam$name)
+
+    dat = read.xlsx("C:/Users/jostou/Downloads/RDBES Data Model CS.xlsx",
+                    sheet = nam[j, "name"])
+
+    dat <- dat[dat$Order %in% 1:900, c("Order", "Field.Name")]
+    #dat$Field.Name <- substr(dat$Field.Name, 3, nchar(dat$Field.Name))
+
+    dat2 <- data.frame(matrix(nrow = 0, ncol = nrow(dat)))
+    names(dat2) <- dat$Field.Name
+
+    assign(
+      x = nam[j, "code"],
+      value = dat2,
+      envir = .GlobalEnv)
+
+    rm(dat, dat2)
+
+  }
+}
+
 LO_fishline_2_rdbes <-
   function(ref_path = "Q:/mynd/RDB/create_RDBES_data/references",
            sampling_scheme = "DNK_Market_Sampling",
@@ -40,17 +75,20 @@ LO_fishline_2_rdbes <-
 
     library(RODBC)
     library(sqldf)
+    library(plyr)
     library(dplyr)
     library(stringr)
     library(haven)
 
-    data_model <- readRDS(paste0(ref_path, "/BaseTypes.rds"))
+    #data_model <- readRDS(paste0(ref_path, "/BaseTypes.rds"))
+    getDataModel("Location")
+
     link <- read.csv(paste0(ref_path, "/link_fishLine_sampling_designs.csv"))
 
     link <- subset(link, DEsamplingScheme == sampling_scheme)
 
-    lo_temp <- filter(data_model, substr(name, 1, 2) == "LO")
-    lo_temp_t <- c("LOrecordType", t(lo_temp$name)[1:nrow(lo_temp)])
+    # lo_temp <- filter(data_model, substr(name, 1, 2) == "LO")
+    # lo_temp_t <- c("LOrecordType", t(lo_temp$name)[1:nrow(lo_temp)])
 
     trips <- unique(link$tripId[!is.na(link$tripId)])
 
@@ -61,7 +99,7 @@ LO_fishline_2_rdbes <-
     tr <- sqlQuery(
       channel,
       paste(
-        "select * FROM dbo.Trip
+        "select * FROM [FishLineDW].dbo.Trip
          WHERE (Trip.year between ", min(years), " and ", max(years) , ")
                 and Trip.tripId in (", paste(trips, collapse = ","),
         ")",
@@ -78,7 +116,7 @@ LO_fishline_2_rdbes <-
     locode <- sqlQuery(
       channel,
       paste(
-        "SELECT harbour, harbourEU FROM L_Harbour",
+        "SELECT harbour, harbourEU FROM [FishLine].dbo.L_Harbour",
         sep = ""
       )
     )
@@ -106,43 +144,50 @@ LO_fishline_2_rdbes <-
     lo$LOid <- "" # To be coded after join with DE and SD
     lo$LOrecordType <- "LO"
 
-    lo$LOlocationName <- ""
-    lo$LOlocationType <- ""
-
-    lo$LOsequenceNumber <- NA   # To be coded after join with DE and SD
+    # lo$LOlocationName <- ""
+    # lo$LOlocationType <- ""
+    #
+    # lo$LOsequenceNumber <- NA   # To be coded after join with DE and SD
     lo$LOclustering <- "N"      # Not used in this scheme
     lo$LOclusterName <- "No"    # Not used in this scheme
 
     lo$LOsampler <-  "Observer" # DTU Aqua selects the locationa
 
-    lo$LOselectionProb <- ""    # Not included for this scheme
-    lo$LOinclusionProb <- ""    # Not included for this scheme
+    # lo$LOselectionProb <- ""    # Not included for this scheme
+    # lo$LOinclusionProb <- ""    # Not included for this scheme
 
     lo$LOunitName <- lo$LOlocode
 
-    lo$LOselectionMethodCluster <- ""  # Not used in this scheme
-    lo$LOnumberTotalClusters <- ""     # Not used in this scheme
-    lo$LOnumberSampledClusters <- ""   # Not used in this scheme
-    lo$LOselectionProbCluster <- ""    # Not used in this scheme
-    lo$LOinclusionProbCluster <- ""    # Not used in this scheme
+    # lo$LOselectionMethodCluster <- ""  # Not used in this scheme
+    # lo$LOnumberTotalClusters <- ""     # Not used in this scheme
+    # lo$LOnumberSampledClusters <- ""   # Not used in this scheme
+    # lo$LOselectionProbCluster <- ""    # Not used in this scheme
+    # lo$LOinclusionProbCluster <- ""    # Not used in this scheme
 
     lo$LOsampled <- "Y"
-    lo$LOreasonNotSampled <- ""           # No non-responses, but we have NULL samples in our DB
+    #lo$LOreasonNotSampled <- ""           # No non-responses, but we have NULL samples in our DB
 
-    if (type == "only_mandatory") {
-      lo_temp_optional <-
-        filter(data_model, substr(name, 1, 2) == "LO" & min == 0)
-      lo_temp_optional_t <-
-        factor(t(lo_temp_optional$name)[1:nrow(lo_temp_optional)])
+    lo <- rbind.fill(LO, lo)
 
-      for (i in levels(lo_temp_optional_t)) {
-        eval(parse(text = paste0("lo$", i, " <- ''")))
+    # if (type == "only_mandatory") {
+    #   lo_temp_optional <-
+    #     filter(data_model, substr(name, 1, 2) == "LO" & min == 0)
+    #   lo_temp_optional_t <-
+    #     factor(t(lo_temp_optional$name)[1:nrow(lo_temp_optional)])
+    #
+    #   for (i in levels(lo_temp_optional_t)) {
+    #     eval(parse(text = paste0("lo$", i, " <- ''")))
+    #
+    #   }
+    # }
 
-      }
-    }
+    #LO <- select(lo, one_of(lo_temp_t), tripId, LOid)
 
-    LO <- select(lo, one_of(lo_temp_t), tripId, LOid)
+    lo <- lo[ , c(names(LO), "tripId", "LOid")]
 
-    return(list(LO, lo_temp, lo_temp_t))
+    lo[is.na(lo) ] <- ""
+
+    #return(list(LO, lo_temp, lo_temp_t))
+    return(list(lo, LO))
 
   }
