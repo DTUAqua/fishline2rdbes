87bb50d3ca761cc42b59cec1fae6a3540b7eaef2
Kirsten Birch Håkansson, DTU Aqua <kih@aqua.dtu.dk>
2023-09-19 12:48:05 +0200

    Næsten færdig fun.

--- R/gear_info_fishline_2_rdbes.R
+++ R/gear_info_fishline_2_rdbes.R
@@ -29,37 +29,57 @@
 
 gear_info_fishline_2_rdbes <-
   function(df = samp) {
-
-    # usethis::use_package("dplyr")
-    # library(stringr)
-    # library(data.table)
-    # library(openxlsx)
-    # library(purrr)
-    # library(lubridate)
-    # library(RODBC)
-    # library(sqldf)
-    # library(knitr)
+    library(dplyr)
+    library(stringr)
+    library(data.table)
+    library(openxlsx)
+    library(purrr)
+    library(lubridate)
+    library(RODBC)
+    library(sqldf)
+    library(knitr)
 
     # Get references ----
 
     # Get refences from https://github.com/ices-eg/RCGs/tree/master/Metiers/Reference_lists ----
     ## Code taken from the same repo
 
-    source(paste0("https://raw.githubusercontent.com/ices-eg/RCGs/master/Metiers/Scripts/Functions/loadAreaList.R"))
+    source(
+      paste0(
+        "https://raw.githubusercontent.com/ices-eg/RCGs/master/Metiers/Scripts/Functions/loadAreaList.R"
+      )
+    )
 
-    url_area <- "https://github.com/ices-eg/RCGs/raw/master/Metiers/Reference_lists/AreaRegionLookup.csv"
+    url_area <-
+      "https://github.com/ices-eg/RCGs/raw/master/Metiers/Reference_lists/AreaRegionLookup.csv"
 
     area_ref <- loadAreaList(url_area)
 
-    source("https://raw.githubusercontent.com/ices-eg/RCGs/master/Metiers/Scripts/Functions/loadMetierList.R")
+    source(
+      "https://raw.githubusercontent.com/ices-eg/RCGs/master/Metiers/Scripts/Functions/loadMetierList.R"
+    )
 
-    url_metier <- "https://github.com/ices-eg/RCGs/raw/master/Metiers/Reference_lists/RDB_ISSG_Metier_list.csv"
+    url_metier <-
+      "https://github.com/ices-eg/RCGs/raw/master/Metiers/Reference_lists/RDB_ISSG_Metier_list.csv"
 
     metier_ref <- loadMetierList(url_metier)
 
-    source("https://raw.githubusercontent.com/ices-eg/RCGs/master/Metiers/Scripts/Functions/loadSpeciesList.R")
+    # Remove >0 metiers
 
-    url_target <- "https://github.com/ices-eg/RCGs/raw/master/Metiers/Reference_lists/Metier%20Subgroup%20Species%202020.xlsx"
+    metier_ref <- subset(metier_ref, mesh != ">0" & sd_mesh == 0)
+
+    # Add years start and end date to relation
+
+    metier_ref$Start_year[is.na(metier_ref$Start_year)] <- 1990
+    metier_ref$End_year[is.na(metier_ref$End_year)] <- year(today())
+
+
+    source(
+      "https://raw.githubusercontent.com/ices-eg/RCGs/master/Metiers/Scripts/Functions/loadSpeciesList.R"
+    )
+
+    url_target <-
+      "https://github.com/ices-eg/RCGs/raw/master/Metiers/Reference_lists/Metier%20Subgroup%20Species%202020.xlsx"
     target_ref <- loadSpeciesList(url_target)
 
 
@@ -72,34 +92,31 @@ gear_info_fishline_2_rdbes <-
 
     print("Adding RCG region")
 
-      channel <- odbcConnect("FishLine")
-      area <- sqlQuery(
-        channel,
-        paste("SELECT DFUArea, areaICES FROM L_DFUArea",
-              sep = ""
-        )
-      )
-      close(channel)
+    channel <- odbcConnect("FishLine")
+    area <- sqlQuery(channel,
+                     paste("SELECT DFUArea, areaICES FROM L_DFUArea",
+                           sep = ""))
+    close(channel)
 
-      df_1 <- left_join(df, area, by = c("dfuArea" = "DFUArea"))
+    df_1 <- left_join(df, area, by = c("dfuArea" = "DFUArea"))
 
-      print(paste("Before area join: ", nrow(df)))
-      print(paste("After area join: ", nrow(df_1)))
+    print(paste("Before area join: ", nrow(df)))
+    print(paste("After area join: ", nrow(df_1)))
 
-      df_1 <- rename(df_1, "area" = "areaICES")
+    df_1 <- rename(df_1, "area" = "areaICES")
 
-      df_2 <- left_join(df_1, area_ref)
+    df_2 <- left_join(df_1, area_ref)
 
-      print(paste("Before RCG region join: ", nrow(df_1)))
-      print(paste("After RCG region join: ", nrow(df_2)))
+    print(paste("Before RCG region join: ", nrow(df_1)))
+    print(paste("After RCG region join: ", nrow(df_2)))
 
-      no_rcg_region <- subset(df_2, is.na(df_2$RCG))
+    no_rcg_region <- subset(df_2, is.na(df_2$RCG))
 
-      print("Station with missing RCG region: ")
+    print("Station with missing RCG region: ")
 
-      print((distinct(no_rcg_region, year, cruise, trip, station, dfuArea, RCG)))
-
-
+    print((
+      distinct(no_rcg_region, year, cruise, trip, station, dfuArea, RCG)
+    ))
 
 
     ## Gear code ----
@@ -107,73 +124,104 @@ gear_info_fishline_2_rdbes <-
     print("Recode Gear codes")
 
 
-      gear_unique_metier <- unique(metier_ref$gear)
-      gear_unique_samp <- unique(df$gearType)
+    gear_unique_metier <- unique(metier_ref$gear)
+    gear_unique_samp <- unique(df$gearType)
 
-      df$gear <- df$gearType
+    df_2$gear <- df_2$gearType
 
-      df$gear[df$gear == "TBN"] <- "OTB" # This is correct TBN is a OTB
+    df_2$gear[df$gear == "TBN"] <-
+      "OTB" # This is correct TBN is a OTB
 
-      # The issues below should be validate
+    # The issues below should be validate
 
-      df$gear[df$gear == "LL"] <- "LLS"  # This is a bit dodgy, since I don't if it is true - ask Hans / Frank
-      df$gear[df$gear == "FIX"] <- "FPO" # This is a bit dodgy, since I don't know what this is
+    df_2$gear[df_2$gearType == "LL"] <-
+      "LLS"  # This is a bit dodgy, since I don't know if this is true - ask Hans / Frank
+    df_2$gear[df_2$gearType == "FIX"] <-
+      "FPO" # This is a bit dodgy, since I don't know if this is true
 
-      gear_no_match <- subset(df, !(gear %in% gear_unique_metier))
+    gear_no_match <- subset(df_2,!(gear %in% gear_unique_metier))
 
-      print("Station with gear codes not in the metier list: ")
+    print("Station with gear codes not in the metier list: ")
 
-      print((select(gear_no_match, year, cruise, trip, station, dfuArea, gearType, gear)))
+    print((
+      select(
+        gear_no_match,
+        year,
+        cruise,
+        trip,
+        station,
+        dfuArea,
+        gearType,
+        gear
+      )
+    ))
+
+    df_2$gear[is.na(df_2$gear)] <- "MIS"
 
     ## Target assemblage ----
 
     print("Adding target assemblage")
 
-      channel <- odbcConnect("FishLine")
-      art <- sqlQuery(
-        channel,
-        paste("SELECT speciesCode, speciesFAO FROM L_Species",
-              sep = ""
-        )
+    channel <- odbcConnect("FishLine")
+    art <- sqlQuery(channel,
+                    paste("SELECT speciesCode, speciesFAO FROM L_Species",
+                          sep = ""))
+    close(channel)
+
+    df_4 <-
+      left_join(df_2, art, by = c("targetSpecies1" = "speciesCode"))
+
+    print(paste("Before species join: ", nrow(df)))
+    print(paste("After species join: ", nrow(df_4)))
+
+    df_4$FAO_species <- df_4$speciesFAO
+
+    df_5 <- left_join(df_4, target_ref)
+
+    df_5 <- rename(df_5, targetSpecies = species_group)
+
+    print("Adding metier6")
+
+    metier_by <-
+      join_by(
+        RCG,
+        year >= Start_year,
+        year <= End_year,
+        gear,
+        targetSpecies == target,
+        meshSize >= m_size_from,
+        meshSize < m_size_to
       )
-      close(channel)
 
-      df_4 <- left_join(df, art, by = c("targetSpecies1" = "speciesCode"))
+    df_6 <- left_join(df_5, metier_ref, metier_by)
 
-      print(paste("Before species join: ", nrow(df)))
-      print(paste("After species join: ", nrow(df_4)))
+    df_6$metier_level_6[is.na(df_6$metier_level_6)] <-
+      "MIS_MIS_0_0_0"
 
-      df_4$FAO_species <- df_4$speciesFAO
-
-      df_5 <- left_join(df_4, target_ref)
-
-
-    df_5 <- left_join(df_4, metier_ref)
+    df_6 <- rename(df_6, metier6 = metier_level_6)
 
     # Code RDBES variables ----
 
-    # nationalFishingActivity <- ""
-    # metier5 <- ""
-    # metier6
-    # gear <- gearType
+    dat <- df_6
+
+    dat$nationalFishingActivity <-
+      "" # Kibi: I don't think we need to code this one. We can just use a relation when estimating
+    dat$metier5 <- "" # Kibi: Optional. It can be deduced from metier6
+    # metier6 - done above
+    # # gear - done above
     # # meshSize - already in data with the correct name
-    # selectionDevice <- "" # TO DO - we have the info in the observer programs
-    # selectionDeviceMeshSize <- "" # Same as above
-    # targetSpecies <-
+    dat$selectionDevice <-
+      "" # TO DO - we have the info in the observer programs for the active gears
+    dat$selectionDeviceMeshSize <- "" # Same as above
+    # # targetSpecies <-
     # # MitigationDevice is not recorded for now, but will come for gillnets in the future
-    # incidentalByCatchMitigationDeviceFirst <- "NotRecorded" #This is T for now
-    # incidentalByCatchMitigationDeviceTargetFirst <- "NotApplicable"
-    # incidentalByCatchMitigationDeviceSecond <- "NotRecorded"
-    # incidentalByCatchMitigationDeviceTargetSecond <- "NotApplicable"
-    # gearDimensions <- numNets*lengthNets
+    dat$incidentalByCatchMitigationDeviceFirst <-
+      "NotRecorded" #This is T for now
+    dat$incidentalByCatchMitigationDeviceTargetFirst <- "NotApplicable"
+    dat$incidentalByCatchMitigationDeviceSecond <- "NotRecorded"
+    dat$incidentalByCatchMitigationDeviceTargetSecond <- "NotApplicable"
+    # dat$gearDimensions <- dat$numNets * dat$lengthNets
 
-
-
-
-
-
-
-
-
+    return(dat)
 
   }
