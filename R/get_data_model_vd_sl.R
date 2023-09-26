723985caa2d545787cc147d40186ab9f7ae1eda3
Kirsten Birch Håkansson <kibi@dtu.dk>
2023-09-26 10:15:41 +0200

    1. version copy of get_data_model

---
+++ R/get_data_model_vd_sl.R
@@ -0,0 +1,45 @@
+#' Title
+#'
+#' @param table_name
+#' @param data_model_version
+#'
+#' @importFrom openxlsx read.xlsx
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_data_model <- function(table_name, data_model_version = "v1_19_18") {
+  ## datamodel
+
+  nam <- data.frame(name = c("Design", "Location", "Temporal Event",
+                             "Species Selection",  "Landing event", "Sample",
+                             "Frequency Measure", "Biological Variable"),
+                    code = c("DE", "LO", "TE", "SS", "LE", "SA", "FM", "BV"))
+
+  if (! table_name %in% nam$name){
+    print("Table Name Not Found In CS Design Model")
+  } else{
+
+    j <- match(table_name, nam$name)
+
+    dat = openxlsx::read.xlsx(paste0("./data/RDBES_Data_Model_CS_", data_model_version, ".xlsx"),
+                    sheet = nam[j, "name"])
+
+    dat <- dat[dat$Order %in% 1:900, c("Order", "Field.Name")]
+    #dat$Field.Name <- substr(dat$Field.Name, 3, nchar(dat$Field.Name))
+
+    dat2 <- data.frame(matrix(nrow = 0, ncol = nrow(dat)))
+    names(dat2) <- dat$Field.Name
+
+    # assign(
+    #   x = nam[j, "code"],
+    #   value = dat2,
+    #   envir = .GlobalEnv)
+
+    # rm(dat, dat2)
+
+  }
+
+  return(dat2)
+}
