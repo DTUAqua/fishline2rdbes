8f3d2bbd2a69596e9b28ffdb898fd87a4af7ef1a
Kirsten Birch Håkansson, DTU Aqua <kih@aqua.dtu.dk>
2024-03-26 11:24:16 +0100

    Update FT_fishline_2_rdbes.R
    Included passive

--- R/FT_fishline_2_rdbes.R
+++ R/FT_fishline_2_rdbes.R
@@ -31,13 +31,13 @@ FT_fishline_2_rdbes <-
     # Input for testing ----
 
 
-    # ref_path <- "Q:/dfad/data/Data/RDBES/sample_data/create_RDBES_data/references/link_fishLine_sampling_designs_2022.csv"
-    # encryptedVesselCode_path <-
-    #   "Q:/dfad/data/Data/RDBES/sample_data/create_RDBES_data/output/for_production"
-    # years <- 2022
-    # sampling_scheme <- "DNK_AtSea_Observer_Active"
-    # data_model_path <-
-    #   "Q:/dfad/data/Data/RDBES/sample_data/create_RDBES_data/input"
+    ref_path <- "Q:/dfad/data/Data/RDBES/sample_data/create_RDBES_data/references/link_fishLine_sampling_designs_2022.csv"
+    encryptedVesselCode_path <-
+      "Q:/dfad/data/Data/RDBES/sample_data/create_RDBES_data/output/for_production"
+    years <- 2022
+    sampling_scheme <- c("DNK_AtSea_Observer_Active", "DNK_AtSea_Observer_passive")
+    data_model_path <-
+      "Q:/dfad/data/Data/RDBES/sample_data/create_RDBES_data/input"
 
     # Set-up ----
 
@@ -54,7 +54,7 @@ FT_fishline_2_rdbes <-
     # Get link ----
     link <- read.csv(ref_path)
 
-    link <- subset(link, DEsamplingScheme == sampling_scheme)
+    link <- subset(link, DEsamplingScheme %in% sampling_scheme)
 
     trips <- unique(link$tripId[!is.na(link$tripId)])
 
