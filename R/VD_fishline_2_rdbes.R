da971a00d1073ba880662e9c7573300eb906d79c
Kirsten Birch Håkansson, DTU Aqua <kih@aqua.dtu.dk>
2024-03-27 10:22:51 +0100

    Included path to data model again

--- R/VD_fishline_2_rdbes.R
+++ R/VD_fishline_2_rdbes.R
@@ -23,13 +23,14 @@
 
 VD_fishline_2_rdbes <-
   function(year = 2022,
-           cruises = c("MON", "SEAS", "IN-HIRT", "IN-LYNG"))
+           cruises = c("MON", "SEAS", "IN-HIRT", "IN-LYNG"),
+           data_model_path = data_model_path)
   {
 
 
     # Input for testing ----
 
-    # data_model_baseTypes_path <- "Q:/mynd/RDB/create_RDBES_data/references"
+    # data_model_path <- "Q:/dfad/data/Data/RDBES/fishline2rdb/data"
     # year <- 2021
     # cruises <- c("MON", "SEAS", "IN-HIRT", "IN-LYNG")
     # type <- "only_mandatory"
@@ -44,7 +45,7 @@ VD_fishline_2_rdbes <-
     library(haven)
 
     # Get data model ----
-    VD <- get_data_model_vd_sl("Vessel Details")
+    VD <- get_data_model_vd_sl("Vessel Details", data_model_path = data_model_path)
 
     # Get needed stuff ----
 
